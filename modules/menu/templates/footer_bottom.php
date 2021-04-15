b9422b9d1362a50310018f9a060fe85460975d35
Nikolay <rachkuw@gmail.com>
2021-04-15 10:14:10 +0300

    Footer bottom

---
+++ modules/menu/templates/footer_bottom.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+
+type: layout
+
+name: Footer
+
+description: Footer menu
+
+*/
+
+?>
+
+<?php
+
+
+
+$menu_filter['link'] = '<a itemprop="url" data-item-id="{id}"  href="{url}">{title}</a>';
+$mt = menu_tree($menu_filter);
+
+if ($mt != false) {
+    print ($mt);
+} else {
+    print lnotif("There are no items in the menu <b>" . $params['menu-name'] . '</b>');
+}
+?>
+

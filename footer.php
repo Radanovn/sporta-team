312278e20bfa014e67991273ec163d159fc465f9
Nikolay <rachkuw@gmail.com>
2021-04-15 17:57:26 +0300

    footer rel and field

--- footer.php
+++ footer.php
@@ -2,94 +2,96 @@
 </div>
 <footer class="footer bg-gray">
     <div class="container">
-        <div class="row no-gutters justify-content-between">
-            <div class="col-md-3 col-xl-3 pr-xl-0 pr-lg-3">
-                <div class="text-center text-md-left">
-                    <a class="navbar-brand" href="#">
-                        <module type="logo" id="logo" template="default" class="d-inline-block align-top" default-text="Sporta"/>
-                    </a>
-                </div>
-                <p class="footer__small-text text-center text-md-left edit safe-mode">The pinnacle of recreational sports is what Sporta-federatie vzw has to offer sports clubs.<br>
-                    The Sporta employees not only have the knowledge and experience, they are particularly keen to support your club activities fourfold.</p>
-                <div class="follow-us-block">
-                    <p class="follow-us-block__title edit safe-mode">Follow us</p>
-                    <module type="social_links" template="sporta-socials"/>
-                </div>
-                <div class="collaboration-block text-center text-md-left edit safe-element">
-                    <p class="collaboration-block__title edit safe-mode">In collaboration with</p>
-                    <div>
-                        <div class="collaboration-block__logo cloneable">
-                            <a href="#">
-                                <img src="<?php print template_url(); ?>images/logo_ethias.png" class="d-inline-block align-top" alt="">
-                            </a>
-                        </div>
-                        <div class="collaboration-block__logo cloneable">
-                            <a href="#">
-                                <img src="<?php print template_url(); ?>images/logo_sport.png" class="d-inline-block align-top" alt="">
-                            </a>
+        <div class="edit nodrop safe-mode" field="sporta-team_footer" rel="global">
+            <div class="row no-gutters justify-content-between">
+                <div class="col-md-3 col-xl-3 pr-xl-0 pr-lg-3">
+                    <div class="text-center text-md-left">
+                        <a class="navbar-brand" href="#">
+                            <module type="logo" id="logo" template="default" class="d-inline-block align-top" default-text="Sporta"/>
+                        </a>
+                    </div>
+                    <p class="footer__small-text text-center text-md-left edit safe-mode">The pinnacle of recreational sports is what Sporta-federatie vzw has to offer sports clubs.<br>
+                        The Sporta employees not only have the knowledge and experience, they are particularly keen to support your club activities fourfold.</p>
+                    <div class="follow-us-block">
+                        <p class="follow-us-block__title edit safe-mode">Follow us</p>
+                        <module type="social_links" template="sporta-socials"/>
+                    </div>
+                    <div class="collaboration-block text-center text-md-left edit safe-element">
+                        <p class="collaboration-block__title edit safe-mode">In collaboration with</p>
+                        <div>
+                            <div class="collaboration-block__logo cloneable">
+                                <a href="#">
+                                    <img src="<?php print template_url(); ?>images/logo_ethias.png" class="d-inline-block align-top" alt="">
+                                </a>
+                            </div>
+                            <div class="collaboration-block__logo cloneable">
+                                <a href="#">
+                                    <img src="<?php print template_url(); ?>images/logo_sport.png" class="d-inline-block align-top" alt="">
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-8">
-                <ul class="list-unstyled footer__menu">
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_1" template="footer"/>
-                    </li>
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_2" template="footer"/>
-                    </li>
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_3" template="footer"/>
-                    </li>
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_4" template="footer"/>
-                    </li>
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_5" template="footer"/>
-                    </li>
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_6" template="footer"/>
-                    </li>
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_7" template="footer"/>
-                    </li>
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_8" template="footer"/>
-                    </li>
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_9" template="footer"/>
-                    </li>
-                    <li>
-                        <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                        <module type="menu" name="footer_menu_10" template="footer"/>
-                    </li>
-                    <li>
-                        <div class="contact-block">
+                <div class="col-md-8">
+                    <ul class="list-unstyled footer__menu">
+                        <li>
                             <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
-                            <module type="btn" template="sporta-buttons" button_style="st-email" />
-                            <module type="btn" template="sporta-buttons" button_style="st-phone" />
-                            <module type="btn" template="sporta-btn-link-p" />
-                            <module type="btn" template="sporta-buttons" button_style="btn-red" />
-                        </div>
-                    </li>
+                            <module type="menu" name="footer_menu_1" template="footer"/>
+                        </li>
+                        <li>
+                            <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                            <module type="menu" name="footer_menu_2" template="footer"/>
+                        </li>
+                        <li>
+                            <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                            <module type="menu" name="footer_menu_3" template="footer"/>
+                        </li>
+                        <li>
+                            <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                            <module type="menu" name="footer_menu_4" template="footer"/>
+                        </li>
+                        <li>
+                            <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                            <module type="menu" name="footer_menu_5" template="footer"/>
+                        </li>
+                        <li>
+                            <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                            <module type="menu" name="footer_menu_6" template="footer"/>
+                        </li>
+                        <li>
+                            <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                            <module type="menu" name="footer_menu_7" template="footer"/>
+                        </li>
+                        <li>
+                            <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                            <module type="menu" name="footer_menu_8" template="footer"/>
+                        </li>
+                        <li>
+                            <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                            <module type="menu" name="footer_menu_9" template="footer"/>
+                        </li>
+                        <li>
+                            <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                            <module type="menu" name="footer_menu_10" template="footer"/>
+                        </li>
+                        <li>
+                            <div class="contact-block">
+                                <module type="btn" template="sporta-buttons" button_style="footer__menu-link" />
+                                <module type="btn" template="sporta-buttons" button_style="st-email" />
+                                <module type="btn" template="sporta-buttons" button_style="st-phone" />
+                                <module type="btn" template="sporta-btn-link-p" />
+                                <module type="btn" template="sporta-buttons" button_style="btn-red" />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer__bottom">
+                <p class="copyright edit safe-mode"><span class="copyright__symbol">©</span> Sporta Team</p>
+                <ul class="list-unstyled d-flex justify-content-end align-items-center footer__bottom-menu">
+                    <module type="menu" name="footer_menu_11" template="footer_bottom"/>
                 </ul>
             </div>
-        </div>
-        <div class="footer__bottom">
-            <p class="copyright edit safe-mode"><span class="copyright__symbol">©</span> Sporta Team</p>
-            <ul class="list-unstyled d-flex justify-content-end align-items-center footer__bottom-menu">
-                <module type="menu" name="footer_menu_11" template="footer_bottom"/>
-            </ul>
         </div>
     </div>
 </footer>

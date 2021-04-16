26e3390b9f5b4235ca82e668f14936d8a94952fb
Nikolay <rachkuw@gmail.com>
2021-04-16 13:30:47 +0300

    slider and blank layout

---
+++ modules/layouts/templates/home-sliders/skin-1.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+
+type: layout
+
+name: skin-1
+
+position: 1
+
+*/
+
+?>
+
+<?php
+if (!$classes['padding_top']) {
+    $classes['padding_top'] = 'p-t-100';
+}
+if (!$classes['padding_bottom']) {
+    $classes['padding_bottom'] = 'p-b-100';
+}
+
+$layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom'] . ' ';
+?>
+
+<section class="section <?php print $layout_classes; ?> hero-section nodrop edit" field="layout-home-sliders-skin-1-<?php print $params['id'] ?>" rel="module">
+    <div id="heroSlider" class="carousel slide" data-ride="carousel" data-interval="false">
+        <ol class="carousel-indicators">
+            <li data-target="#heroSlider" data-slide-to="0" class="active"></li>
+            <li data-target="#heroSlider" data-slide-to="1"></li>
+            <li data-target="#heroSlider" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row no-gutters">
+                    <div class="col-12 col-md-4 order-1 slide__text">
+                        <h1 class="slide__text-title">YOU THE SPORT, WE THE <span class="text-red">SUPPORT</span></h1>
+                    </div>
+                    <div class="col-12 col-md-8 order-0 order-md-1 slide__img">
+                        <picture>
+                            <source srcset="<?php print template_url(); ?>images/slide1-mobile.jpg" media="(max-width: 767px)">
+                            <img src="<?php print template_url(); ?>images/slide1.jpg" alt="" class="img-cover">
+                        </picture>
+                        <div class="slide__img-decor-left bg-yellow d-none d-md-block"></div>
+                        <div class="slide__img-decor-right d-none d-md-block"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row no-gutters">
+                    <div class="col-12 col-md-4 order-1 slide__text">
+                        <div class="slide__text-title">YOU THE SPORT, WE THE <span class="text-red">SUPPORT</span></div>
+                    </div>
+                    <div class="col-12 col-md-8 order-0 order-md-1 slide__img">
+                        <picture>
+                            <source srcset="<?php print template_url(); ?>images/slide2-mobile.jpg" media="(max-width: 767px)">
+                            <img src="<?php print template_url(); ?>images/slide2.jpg" alt="" class="img-cover">
+                        </picture>
+                        <div class="slide__img-decor-left bg-green d-none d-md-block"></div>
+                        <div class="slide__img-decor-right d-none d-md-block"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row no-gutters">
+                    <div class="col-12 col-md-4 order-1 slide__text">
+                        <div class="slide__text-title">YOU THE SPORT, WE THE <span class="text-green">SUPPORT</span></div>
+                    </div>
+                    <div class="col-12 col-md-8 order-0 order-md-1 slide__img">
+                        <picture>
+                            <source srcset="<?php print template_url(); ?>images/slide3-mobile.jpg" media="(max-width: 767px)">
+                            <img src="<?php print template_url(); ?>images/slide3.jpg" alt="" class="img-cover">
+                        </picture>
+                        <div class="slide__img-decor-left bg-red d-none d-md-block"></div>
+                        <div class="slide__img-decor-right d-none d-md-block"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-next d-none d-md-flex" href="#heroSlider" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <div class="circles circles--bottom-right d-none d-lg-block">
+        <img src="<?php print template_url(); ?>modules/layouts/templates/svg/home-slider.svg">
+    </div>
+</section>

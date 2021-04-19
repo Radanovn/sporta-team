308b1c22eba919a31d5f051d01bec7d30d5fc34d
Nikolay <rachkuw@gmail.com>
2021-04-19 18:03:44 +0300

    update

--- layouts/contact.php
+++ layouts/contact.php
@@ -12,9 +12,13 @@ description: Contacts
 
 ?>
 <?php include template_dir() . "header.php"; ?>
+
+<div class="edit main-content" rel="content" field="sporta-team_contact">
     <module type="layouts" template="contact/skin-1"/>
     <module type="layouts" template="contact/skin-2"/>
     <module type="layouts" template="contact/skin-3"/>
     <module type="layouts" template="contact/skin-4"/>
+</div>
+
 <?php include template_dir() . "footer.php"; ?>
 

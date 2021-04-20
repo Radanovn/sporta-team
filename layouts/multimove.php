4cce86472642258d1424801d9bdc64161f0526d4
Nikolay <rachkuw@gmail.com>
2021-04-20 14:32:42 +0300

    update

--- layouts/multimove.php
+++ layouts/multimove.php
@@ -66,9 +66,7 @@ description: Multimove
                         <h3 class="about-card__title">Voordelig aansluitingstar</h3>
                         <p class="about-card__text  about-card__text--max-width">De kinderen en lesgevers kan je aansluiten aan het voordelige M-tarief dat slechts €5/jaar bedraagt. Hiermee zijn zij verzekerd voor BA en LO.</p>
                         <div class="about-card__wrap-btn">
-                            <a href="#" class="btn btn-animate btn-red">
-                                YES, IK SLUIT ME AAN
-                            </a>
+                           <module type="btn" template="sporta-buttons" button_style="btn btn-animate btn-red">
                         </div>
                     </div>
                 </div>
@@ -102,7 +100,7 @@ description: Multimove
                                 </ul>
                             </div>
                             <div class="card-hover-block__footer">
-                                <a href="#" class="btn btn-animate">more info</a>
+                                <module type="btn" template="sporta-buttons" button_style="default">
                             </div>
                         </div>
                         <a href="#" class="d-block card-img-top rounded-0">
@@ -119,14 +117,7 @@ description: Multimove
                             <p class="card-text">Every two years, all trainers in Flanders gather for a day full of workshops, practical sessions, refresher courses and information about new developments, trends and tips. Both for certi-fied and (yet) non-qualified trainers or sports coaches.</p>
                         </div>
                         <div class="card-footer border-0 bg-transparent rounded-0">
-                            <a href="#" class="card-link link color-yellow">
-                                Meer info
-                                <span class="link__arrow">
-                                   <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                       <path d="M1.70711 0.370774C1.31658 -0.0197506 0.683417 -0.0197506 0.292893 0.370774C-0.0976315 0.761298 -0.0976314 1.39446 0.292893 1.78499L1.70711 0.370774ZM5 5.07788L5.70711 5.78499C5.89464 5.59745 6 5.3431 6 5.07788C6 4.81266 5.89464 4.55831 5.70711 4.37077L5 5.07788ZM0.292893 8.37077C-0.0976311 8.7613 -0.0976311 9.39446 0.292893 9.78499C0.683418 10.1755 1.31658 10.1755 1.70711 9.78499L0.292893 8.37077ZM0.292893 1.78499L4.29289 5.78499L5.70711 4.37077L1.70711 0.370774L0.292893 1.78499ZM4.29289 4.37077L0.292893 8.37077L1.70711 9.78499L5.70711 5.78499L4.29289 4.37077Z" fill="#000000"/>
-                                   </svg>
-                               </span>
-                            </a>
+                            <module type="btn" template="sporta-card-links-with-icons" button_style="red">
                         </div>
                     </div>
                 </div>
@@ -141,7 +132,8 @@ description: Multimove
                                 </ul>
                             </div>
                             <div class="card-hover-block__footer">
-                                <a href="#" class="btn btn-animate">more info</a>
+                                <module type="btn" template="sporta-buttons" button_style="default">
+
                             </div>
                         </div>
                         <a href="#" class="d-block card-img-top rounded-0">
@@ -158,14 +150,7 @@ description: Multimove
                             <p class="card-text">Every two years, all trainers in Flanders gather for a day full of workshops, practical sessions, refresher courses and information about new developments, trends and tips. Both for certi-fied and (yet) non-qualified trainers or sports coaches.</p>
                         </div>
                         <div class="card-footer border-0 bg-transparent">
-                            <a href="#" class="card-link link color-yellow">
-                                Meer info
-                                <span class="link__arrow">
-                                   <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                       <path d="M1.70711 0.370774C1.31658 -0.0197506 0.683417 -0.0197506 0.292893 0.370774C-0.0976315 0.761298 -0.0976314 1.39446 0.292893 1.78499L1.70711 0.370774ZM5 5.07788L5.70711 5.78499C5.89464 5.59745 6 5.3431 6 5.07788C6 4.81266 5.89464 4.55831 5.70711 4.37077L5 5.07788ZM0.292893 8.37077C-0.0976311 8.7613 -0.0976311 9.39446 0.292893 9.78499C0.683418 10.1755 1.31658 10.1755 1.70711 9.78499L0.292893 8.37077ZM0.292893 1.78499L4.29289 5.78499L5.70711 4.37077L1.70711 0.370774L0.292893 1.78499ZM4.29289 4.37077L0.292893 8.37077L1.70711 9.78499L5.70711 5.78499L4.29289 4.37077Z" fill="#000000"/>
-                                   </svg>
-                               </span>
-                            </a>
+                            <module type="btn" template="sporta-card-links-with-icons" button_style="green">
                         </div>
                     </div>
                 </div>
@@ -180,7 +165,7 @@ description: Multimove
                                 </ul>
                             </div>
                             <div class="card-hover-block__footer">
-                                <a href="#" class="btn btn-animate">more info</a>
+                                <module type="btn" template="sporta-buttons" button_style="default">
                             </div>
                         </div>
                         <a href="#" class="d-block card-img-top rounded-0">
@@ -197,14 +182,7 @@ description: Multimove
                             <p class="card-text">Every two years, all trainers in Flanders gather for a day full of workshops, practical sessions, refresher courses and information about new developments, trends and tips. Both for certi-fied and (yet) non-qualified trainers or sports coaches.</p>
                         </div>
                         <div class="card-footer border-0 bg-transparent">
-                            <a href="#" class="card-link link color-yellow">
-                                Meer info
-                                <span class="link__arrow">
-                                   <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                       <path d="M1.70711 0.370774C1.31658 -0.0197506 0.683417 -0.0197506 0.292893 0.370774C-0.0976315 0.761298 -0.0976314 1.39446 0.292893 1.78499L1.70711 0.370774ZM5 5.07788L5.70711 5.78499C5.89464 5.59745 6 5.3431 6 5.07788C6 4.81266 5.89464 4.55831 5.70711 4.37077L5 5.07788ZM0.292893 8.37077C-0.0976311 8.7613 -0.0976311 9.39446 0.292893 9.78499C0.683418 10.1755 1.31658 10.1755 1.70711 9.78499L0.292893 8.37077ZM0.292893 1.78499L4.29289 5.78499L5.70711 4.37077L1.70711 0.370774L0.292893 1.78499ZM4.29289 4.37077L0.292893 8.37077L1.70711 9.78499L5.70711 5.78499L4.29289 4.37077Z" fill="#000000"/>
-                                   </svg>
-                               </span>
-                            </a>
+                            <module type="btn" template="sporta-card-links-with-icons" button_style="yellow">
                         </div>
                     </div>
                 </div>
@@ -224,14 +202,7 @@ description: Multimove
                         <h4 class="about-card__title">Opleidingen en bijscholing<br class="d-none d-lg-inline"> voor multi&#8209;movebegeleiders</h4>
                         <p class="about-card__text  about-card__text--max-width">De kinderen en lesgevers kan je aansluiten aan het voordelige M-tarief dat slechts €5/jaar bedraagt. Hiermee zijn zij verzekerd voor BA en LO.</p>
                         <div class="about-card__wrap-link">
-                            <a href="#" class="link color-red text-decoration-none">
-                                Find out more
-                                <span class="link__arrow">
-                                   <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                       <path d="M1.70711 0.370774C1.31658 -0.0197506 0.683417 -0.0197506 0.292893 0.370774C-0.0976315 0.761298 -0.0976314 1.39446 0.292893 1.78499L1.70711 0.370774ZM5 5.07788L5.70711 5.78499C5.89464 5.59745 6 5.3431 6 5.07788C6 4.81266 5.89464 4.55831 5.70711 4.37077L5 5.07788ZM0.292893 8.37077C-0.0976311 8.7613 -0.0976311 9.39446 0.292893 9.78499C0.683418 10.1755 1.31658 10.1755 1.70711 9.78499L0.292893 8.37077ZM0.292893 1.78499L4.29289 5.78499L5.70711 4.37077L1.70711 0.370774L0.292893 1.78499ZM4.29289 4.37077L0.292893 8.37077L1.70711 9.78499L5.70711 5.78499L4.29289 4.37077Z" fill="#000000"/>
-                                   </svg>
-                               </span>
-                            </a>
+                            <module type="btn" template="sporta-card-links-with-icons" button_style="red text-decoration-none">
                         </div>
                     </div>
                 </div>
@@ -282,14 +253,7 @@ description: Multimove
                             <li>Multimove-lied</li>
                         </ul>
                         <div class="about-card__wrap-link">
-                            <a href="#" class="link color-green text-decoration-none">
-                                Find out more
-                                <span class="link__arrow">
-                                   <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                       <path d="M1.70711 0.370774C1.31658 -0.0197506 0.683417 -0.0197506 0.292893 0.370774C-0.0976315 0.761298 -0.0976314 1.39446 0.292893 1.78499L1.70711 0.370774ZM5 5.07788L5.70711 5.78499C5.89464 5.59745 6 5.3431 6 5.07788C6 4.81266 5.89464 4.55831 5.70711 4.37077L5 5.07788ZM0.292893 8.37077C-0.0976311 8.7613 -0.0976311 9.39446 0.292893 9.78499C0.683418 10.1755 1.31658 10.1755 1.70711 9.78499L0.292893 8.37077ZM0.292893 1.78499L4.29289 5.78499L5.70711 4.37077L1.70711 0.370774L0.292893 1.78499ZM4.29289 4.37077L0.292893 8.37077L1.70711 9.78499L5.70711 5.78499L4.29289 4.37077Z" fill="#000000"/>
-                                   </svg>
-                               </span>
-                            </a>
+                            <module type="btn" template="sporta-card-links-with-icons" button_style="green text-decoration-none">
                         </div>
                     </div>
                 </div>

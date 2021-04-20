7d99fb8e857186c445728220743442034d2b22f5
Nikolay <rachkuw@gmail.com>
2021-04-20 13:35:02 +0300

    healthy sports

---
+++ modules/layouts/templates/healthy-sports/skin-2.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+
+type: layout
+
+name: Healthy sports 2
+
+position: 2
+
+*/
+
+?>
+<section class="section section-participate section-healthy-sports-exercise edit safe-mode nodrop" field="layout-healthy-sports-skin-2-<?php print $params['id'] ?>" rel="content">
+    <div class="container position-relative">
+        <div class="circles circles--top-right d-none d-lg-block">
+            <img src="<?php print template_url(); ?>modules/layouts/templates/svg/healthy-sports/skin-1.svg">
+        </div>
+        <div class="row">
+            <div class="col-lg-9 mx-auto">
+                <div class="section__block-title text-sm-center">
+                    <h2>Healthy exercise</h2>
+                    <p class="lead-text">The pinnacle of healthy exercise ... is informing yourself about how to prepare and keep your body stamp ready for sports. Especially when the test of time starts to nibble on that body. Sport is healthy, it is the epitome of clichéd statements and at the same time a credo that the Sporta federation shouts out loudly. Of course you are fat with that if things go wrong during that exercise.</p>
+                </div>
+            </div>
+        </div>
+        <div class="wrap-cards">
+            <div class="wrap-card__item">
+                <div class="card border-0 color-yellow">
+                    <div class="card-header bg-transparent border-0">
+                        <h5 class="card-title m-0 h-100">
+                        <span class="card-header__icon">
+                            <img src="<?php print template_url(); ?>modules/layouts/templates/svg/healthy-sports/skin-2.svg">
+                        </span>
+                            <span>Geestig gezond sporten</span>
+                        </h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipis-cing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur.</p>
+                    </div>
+                    <div class="card-footer bg-transparent border-0">
+                        <module type="btn" template="sporta-links-with-icons">
+                    </div>
+                </div>
+            </div>
+            <div class="wrap-card__item order-lg-3">
+                <div class="card border-0 color-red">
+                    <div class="card-header bg-transparent border-0">
+                        <h5 class="card-title m-0 h-100">
+                        <span class="card-header__icon">
+                            <img src="<?php print template_url(); ?>modules/layouts/templates/svg/healthy-sports/skin-3.svg">
+                        </span>
+                            <span>Medische commissie</span>
+                        </h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipis-cing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur.</p>
+                    </div>
+                    <div class="card-footer bg-transparent border-0">
+                        <module type="btn" template="sporta-links-with-icons">
+                    </div>
+                </div>
+            </div>
+            <div class="wrap-card__item order-lg-2">
+                <div class="card border-0 color-green">
+                    <div class="card-header bg-transparent border-0">
+                        <h5 class="card-title m-0 h-100">
+                        <span class="card-header__icon">
+                            <img src="<?php print template_url(); ?>modules/layouts/templates/svg/healthy-sports/skin-4.svg">
+                        </span>
+                            <span>Antidoping</span>
+                        </h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipis-cing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur.</p>
+                    </div>
+                    <div class="card-footer bg-transparent border-0">
+                        <module type="btn" template="sporta-links-with-icons">
+                    </div>
+                </div>
+            </div>
+            <div class="wrap-card__item order-lg-1">
+                <div class="card border-0 color-green">
+                    <div class="card-header bg-transparent border-0">
+                        <h5 class="card-title m-0 h-100">
+                        <span class="card-header__icon">
+                            <img src="<?php print template_url(); ?>modules/layouts/templates/svg/healthy-sports/skin-5.svg">
+                        </span>
+                            <span>Sportkeuring</span>
+                        </h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipis-cing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur.</p>
+                    </div>
+                    <div class="card-footer bg-transparent border-0">
+                        <module type="btn" template="sporta-links-with-icons">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ modules/layouts/templates/healthy-sports/skin-3.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+
+type: layout
+
+name: Healthy sports 3
+
+position: 3
+
+*/
+
+?>
+<section class="section section-member bg-gray section-healthy-sports-ethical edit safe-mode nodrop" field="layout-healthy-sports-skin-3-<?php print $params['id'] ?>" rel="content">
+    <div class="container position-relative">
+        <div class="circles circles--bottom-right d-none d-lg-block">
+            <img src="<?php print template_url(); ?>modules/layouts/templates/svg/healthy-sports/skin-6.svg">
+        </div>
+        <div class="circles circles--top-left d-none d-lg-block">
+            <img src="<?php print template_url(); ?>modules/layouts/templates/svg/healthy-sports/skin-7.svg">
+        </div>
+        <div class="row no-gutters">
+            <div class="col-lg-9 mx-auto">
+                <div class="section__block-title text-sm-center">
+                    <h2>Ethical sports</h2>
+                    <p class="lead-text">The pinnacle of healthy exercise ... is informing yourself about how to prepare and keep your body stamp ready for sports. Especially when the test of time starts to nibble on that body. Sport is healthy, it is the epitome of clichéd statements and at the same time a credo that the Sporta federation shouts out loudly. Of course you are fat with that if things go wrong during that exercise.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-11 col-lg-12 col-xl-11 mx-auto">
+                <div class="row wrapper-cards">
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card color-red text-center border-0">
+                            <div class="card-header bg-transparent border-0">
+                                <h5 class="card-title">Witty healthy sports</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipis-cing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur.</p>
+                            </div>
+                            <div class="card-footer bg-transparent border-top-0">
+                                <module type="btn" template="sporta-links-with-icons">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card color-yellow text-center border-0">
+                            <div class="card-header bg-transparent border-0">
+                                <h5 class="card-title">Sports inspection</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipis-cing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur.</p>
+                            </div>
+                            <div class="card-footer bg-transparent border-top-0">
+                                <module type="btn" template="sporta-links-with-icons">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card color-green text-center border-0">
+                            <div class="card-header bg-transparent border-0">
+                                <h5 class="card-title">Anti-<br class="d-none d-md-inline">doping</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipis-cing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur.</p>
+                            </div>
+                            <div class="card-footer bg-transparent border-top-0">
+                                <module type="btn" template="sporta-links-with-icons">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card color-green text-center border-0">
+                            <a href="#" class="card-body-link">
+                                <div class="card-header bg-transparent border-0">
+                                    <h5 class="card-title">TIP!</h5>
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipis-cing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur.</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

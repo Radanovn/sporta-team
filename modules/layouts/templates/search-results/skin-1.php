d7bda4f1ef1e078eb8e43ee6d52934924a3c2be6
Nikolay <rachkuw@gmail.com>
2021-04-21 17:50:05 +0300

    Search results page layout

---
+++ modules/layouts/templates/search-results/skin-1.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+
+type: layout
+
+name: Search results 1
+
+position: 1
+
+*/
+
+?>
+<section class="section section-search-results edit safe-mode nodrop" field="layout-search-results-skin-1-<?php print $params['id'] ?>" rel="content">
+    <div class="container position-relative">
+        <div class="circles circles--top-left d-none d-lg-block">
+            <img src="<?php print template_url(); ?>modules/layouts/svg/search-results/skin-1.svg">
+        </div>
+        <div class="circles circles--right d-none d-lg-block">
+            <img src="<?php print template_url(); ?>modules/layouts/svg/search-results/skin-2.svg">
+        </div>
+        <div class="circles circles--bottom-left d-none d-lg-block">
+            <img src="<?php print template_url(); ?>modules/layouts/svg/search-results/skin-3.svg">
+        </div>
+        <div class="row no-gutters">
+            <div class="col-lg-9 mx-auto">
+                <h1 class="search-results__title mb-0">6 results found for "search word"</h1>
+                <article class="search-results__item">
+                    <h4 class="search-results__item-title"><a href="#">Titel page (clickable)</a></h4>
+                    <p class="search-results__item-content">De clubbestuurder logt in via <a href="#">search word</a> klikt in de clubomgeving door naar de menu ‘Verzekering’ – ‘Nieuwe Aangifte’ en volgt de stappen die daar staan. Op datzelfde moment ontvang je van Sporta een bevestigingsbrief.</p>
+                </article>
+                <article class="search-results__item">
+                    <h4 class="search-results__item-title"><a href="#">Titel page</a></h4>
+                    <p class="search-results__item-content">De clubbestuurder logt in via <a href="#">search word</a> klikt in de clubomgeving door naar de menu ‘Verzekering’ – ‘Nieuwe Aangifte’ en volgt de stappen die daar staan. Op datzelfde moment ontvang je van Sporta een bevestigingsbrief.</p>
+                </article>
+                <article class="search-results__item">
+                    <h4 class="search-results__item-title"><a href="#">Titel page</a></h4>
+                    <p class="search-results__item-content">De clubbestuurder logt in via <a href="#">search word</a> klikt in de clubomgeving door naar de menu ‘Verzekering’ – ‘Nieuwe Aangifte’ en volgt de stappen die daar staan. Op datzelfde moment ontvang je van Sporta een bevestigingsbrief.</p>
+                </article>
+                <article class="search-results__item">
+                    <h4 class="search-results__item-title"><a href="#">Titel page</a></h4>
+                    <p class="search-results__item-content">De clubbestuurder logt in via <a href="#">search word</a> klikt in de clubomgeving door naar de menu ‘Verzekering’ – ‘Nieuwe Aangifte’ en volgt de stappen die daar staan. Op datzelfde moment ontvang je van Sporta een bevestigingsbrief.</p>
+                </article>
+                <article class="search-results__item">
+                    <h4 class="search-results__item-title"><a href="#">Titel page</a></h4>
+                    <p class="search-results__item-content">De clubbestuurder logt in via <a href="#">search word</a> klikt in de clubomgeving door naar de menu ‘Verzekering’ – ‘Nieuwe Aangifte’ en volgt de stappen die daar staan. Op datzelfde moment ontvang je van Sporta een bevestigingsbrief.</p>
+                </article>
+                <article class="search-results__item">
+                    <h4 class="search-results__item-title"><a href="#">Titel page</a></h4>
+                    <p class="search-results__item-content">De clubbestuurder logt in via <a href="#">search word</a> klikt in de clubomgeving door naar de menu ‘Verzekering’ – ‘Nieuwe Aangifte’ en volgt de stappen die daar staan. Op datzelfde moment ontvang je van Sporta een bevestigingsbrief.</p>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>

7ccc5a90cccdb528279179801686bdac0216a900
Nikolay <rachkuw@gmail.com>
2021-04-21 15:27:53 +0300

    club vizit page buttons

--- layouts/club-vizit.php
+++ layouts/club-vizit.php
@@ -34,17 +34,7 @@ description: Club vizit
     <section class="section section-about section-custom-accompaniment">
         <div class="container position-relative">
             <div class="circles circles--top-left d-none d-lg-block">
-                <svg width="36" height="34" viewBox="0 0 36 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="3.3646" cy="3.20945" r="2.78745" fill="#52B392"/>
-                    <circle cx="17.1551" cy="3.20945" r="2.78745" fill="#52B392"/>
-                    <circle cx="32.4128" cy="3.20945" r="2.78745" fill="#52B392"/>
-                    <circle cx="32.4128" cy="17" r="2.78745" fill="#52B392"/>
-                    <circle cx="17.1551" cy="17" r="2.78745" fill="#52B392"/>
-                    <circle cx="3.3646" cy="17" r="2.78745" fill="#52B392"/>
-                    <circle cx="3.3646" cy="30.7905" r="2.78745" fill="#52B392"/>
-                    <circle cx="17.1551" cy="30.7905" r="2.78745" fill="#52B392"/>
-                    <circle cx="32.4128" cy="30.7905" r="2.78745" fill="#52B392"/>
-                </svg>
+                <img src="<?php print template_url(); ?>modules/layouts/svg/club-vizit/skin-1.svg">
             </div>
             <div class="row">
                 <div class="col-lg-9 mx-lg-auto">
@@ -59,33 +49,13 @@ description: Club vizit
             </div>
             <div class="row no-gutters about-card">
                 <div class="circles circles--top-right d-none d-lg-block">
-                    <svg width="35" height="34" viewBox="0 0 35 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="3.03745" cy="2.78745" r="2.78745" fill="#F6D278"/>
-                        <circle cx="16.828" cy="2.78745" r="2.78745" fill="#F6D278"/>
-                        <circle cx="32.0855" cy="2.78745" r="2.78745" fill="#F6D278"/>
-                        <circle cx="32.0855" cy="16.578" r="2.78745" fill="#F6D278"/>
-                        <circle cx="16.828" cy="16.578" r="2.78745" fill="#F6D278"/>
-                        <circle cx="3.03745" cy="16.578" r="2.78745" fill="#F6D278"/>
-                        <circle cx="3.03745" cy="30.3685" r="2.78745" fill="#F6D278"/>
-                        <circle cx="16.828" cy="30.3685" r="2.78745" fill="#F6D278"/>
-                        <circle cx="32.0855" cy="30.3685" r="2.78745" fill="#F6D278"/>
-                    </svg>
+                    <img src="<?php print template_url(); ?>modules/layouts/svg/club-vizit/skin-2.svg">
                 </div>
                 <div class="col-lg-6">
                     <div class="about-card__img">
                         <div class="rect rect--top-left rect--green d-none d-lg-block"></div>
                         <div class="circles circles--bottom-left d-none d-lg-block">
-                            <svg width="35" height="34" viewBox="0 0 35 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="3.03745" cy="2.78745" r="2.78745" fill="#52B392"/>
-                                <circle cx="16.828" cy="2.78745" r="2.78745" fill="#52B392"/>
-                                <circle cx="32.0855" cy="2.78745" r="2.78745" fill="#52B392"/>
-                                <circle cx="32.0855" cy="16.578" r="2.78745" fill="#52B392"/>
-                                <circle cx="16.828" cy="16.578" r="2.78745" fill="#52B392"/>
-                                <circle cx="3.03745" cy="16.578" r="2.78745" fill="#52B392"/>
-                                <circle cx="3.03745" cy="30.3685" r="2.78745" fill="#52B392"/>
-                                <circle cx="16.828" cy="30.3685" r="2.78745" fill="#52B392"/>
-                                <circle cx="32.0855" cy="30.3685" r="2.78745" fill="#52B392"/>
-                            </svg>
+                            <img src="<?php print template_url(); ?>modules/layouts/svg/club-vizit/skin-3.svg">
                         </div>
                         <picture>
                             <source srcset="<?php print template_url(); ?>images/vizit-img-mobile.jpg" media="(max-width: 479px)">
@@ -99,14 +69,7 @@ description: Club vizit
                         <h3 class="about-card__title">We vizit you</h3>
                         <p class="about-card__text  about-card__text--max-width">Specifically, a club visit works like this: a Sporta employee visits your club to meet personally and to listen to the questions and feedback your club has. In addition, during the club visit there is room to view the possibilities of the services of Sporta together, so that your club makes optimal use of the Sporta advantages. Together we invest in your club activities!</p>
                         <div class="about-card__wrap-link">
-                            <a href="#" class="link color-red text-decoration-none">
-                                Find out more
-                                <span class="link__arrow">
-                                   <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                       <path d="M1.70711 0.370774C1.31658 -0.0197506 0.683417 -0.0197506 0.292893 0.370774C-0.0976315 0.761298 -0.0976314 1.39446 0.292893 1.78499L1.70711 0.370774ZM5 5.07788L5.70711 5.78499C5.89464 5.59745 6 5.3431 6 5.07788C6 4.81266 5.89464 4.55831 5.70711 4.37077L5 5.07788ZM0.292893 8.37077C-0.0976311 8.7613 -0.0976311 9.39446 0.292893 9.78499C0.683418 10.1755 1.31658 10.1755 1.70711 9.78499L0.292893 8.37077ZM0.292893 1.78499L4.29289 5.78499L5.70711 4.37077L1.70711 0.370774L0.292893 1.78499ZM4.29289 4.37077L0.292893 8.37077L1.70711 9.78499L5.70711 5.78499L4.29289 4.37077Z" fill="#000000"/>
-                                   </svg>
-                               </span>
-                            </a>
+                            <module type="btn" template="sporta-card-links-with-icons" button-style="red text-decoration-none" button_text="Find out more">
                         </div>
                     </div>
                 </div>
@@ -139,17 +102,7 @@ description: Club vizit
                     <div class="about-card__img">
                         <div class="rect rect--top-left rect--yellow d-none d-lg-block"></div>
                         <div class="circles circles--bottom-left d-none d-lg-block">
-                            <svg width="35" height="34" viewBox="0 0 35 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="3.03745" cy="2.78745" r="2.78745" fill="#F6D278"/>
-                                <circle cx="16.828" cy="2.78745" r="2.78745" fill="#F6D278"/>
-                                <circle cx="32.0855" cy="2.78745" r="2.78745" fill="#F6D278"/>
-                                <circle cx="32.0855" cy="16.578" r="2.78745" fill="#F6D278"/>
-                                <circle cx="16.828" cy="16.578" r="2.78745" fill="#F6D278"/>
-                                <circle cx="3.03745" cy="16.578" r="2.78745" fill="#F6D278"/>
-                                <circle cx="3.03745" cy="30.3685" r="2.78745" fill="#F6D278"/>
-                                <circle cx="16.828" cy="30.3685" r="2.78745" fill="#F6D278"/>
-                                <circle cx="32.0855" cy="30.3685" r="2.78745" fill="#F6D278"/>
-                            </svg>
+                            <img src="<?php print template_url(); ?>modules/layouts/svg/club-vizit/skin-4.svg">
                         </div>
                         <picture>
                             <source srcset="<?php print template_url(); ?>images/vizit-img-3-mobile.jpg" media="(max-width: 479px)">
@@ -164,14 +117,7 @@ description: Club vizit
                         <h3 class="about-card__title">Short & Powerful</h3>
                         <p class="about-card__text  about-card__text--max-width">The time investment consciously remains minimal. It is therefore not necessary to prepare or find out certain things for the club visit. In order to work effectively, we ask that the number of board members at the club visit be limited to 2 people.<br>It is recommended that one of those two persons<br class="d-sm-none"> is familiar with <a href="#" class="text-green font-weight-bold">My Account</a>.</p>
                         <div class="about-card__wrap-link">
-                            <a href="#" class="link color-red text-decoration-none">
-                                Find out more
-                                <span class="link__arrow">
-                                   <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                       <path d="M1.70711 0.370774C1.31658 -0.0197506 0.683417 -0.0197506 0.292893 0.370774C-0.0976315 0.761298 -0.0976314 1.39446 0.292893 1.78499L1.70711 0.370774ZM5 5.07788L5.70711 5.78499C5.89464 5.59745 6 5.3431 6 5.07788C6 4.81266 5.89464 4.55831 5.70711 4.37077L5 5.07788ZM0.292893 8.37077C-0.0976311 8.7613 -0.0976311 9.39446 0.292893 9.78499C0.683418 10.1755 1.31658 10.1755 1.70711 9.78499L0.292893 8.37077ZM0.292893 1.78499L4.29289 5.78499L5.70711 4.37077L1.70711 0.370774L0.292893 1.78499ZM4.29289 4.37077L0.292893 8.37077L1.70711 9.78499L5.70711 5.78499L4.29289 4.37077Z" fill="#000000"/>
-                                   </svg>
-                               </span>
-                            </a>
+                            <module type="btn" template="sporta-card-links-with-icons" button-style="red text-decoration-none" button_text="Find out more">
                         </div>
                     </div>
                 </div>

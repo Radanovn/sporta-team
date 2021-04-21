cf56e5466d1b88774c5a94c2b396f9b1948d8884
Nikolay <rachkuw@gmail.com>
2021-04-21 18:04:14 +0300

    404 page layouts

--- 404.php
+++ 404.php
@@ -14,16 +14,7 @@ description: 404
 <?php include template_dir() . "header.php"; ?>
 
 <div class="edit main-content" rel="content" field="sporta-404_content">
-    <section class="section section-error-404">
-        <div class="container">
-            <div class="text-center">
-                <img src="<?php print template_url(); ?>images/404-img.svg" alt="">
-                <h4>Something went wrong (</h4>
-                <p>Page not found, try going pack to home page.</p>
-                <a href="/" class="btn btn-red btn-animate">BAck to home</a>
-            </div>
-        </div>
-    </section>
+    <module type="layouts" template="404"/>
 </div>
 
 <?php include template_dir() . "footer.php"; ?>
---
+++ modules/layouts/templates/404.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+
+type: layout
+
+name: 404
+
+position: 30
+
+*/
+
+?>
+<section class="section section-error-404 nodrop edit" field="layout-404-<?php print $params['id'] ?>" rel="content">
+    <div class="container">
+        <div class="text-center">
+            <img src="<?php print template_url(); ?>images/404-img.svg" alt="">
+            <h4>Something went wrong (</h4>
+            <p>Page not found, try going pack to home page.</p>
+            <module type="btn" template="sporta-buttons" button_style="btn btn-animate btn-red" button_text="Back to home"/>
+        </div>
+    </div>
+</section>

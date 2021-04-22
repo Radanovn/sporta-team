ab0ff17b889f1fdcd62e94400b3ffc1503fbda54
Nikolay <rachkuw@gmail.com>
2021-04-22 13:43:17 +0300

    other elements page - layouts

--- modules/posts/templates/news-6.php
+++ modules/posts/templates/news-6.php
@@ -10,25 +10,24 @@ description: news
 
 */
 ?>
-<div class="row cards-wrap mx-lg-auto">
+<div class="row wrap-cards">
     <?php if (!empty($data)): ?>
         <?php foreach ($data as $key => $item): ?>
             <?php
             $itemData = content_data($item['id']);
             $itemTags = content_tags($item['id']);
             ?>
-            <div class="col-lg-4">
-                <div class="card border-0 bg-transparent color-yellow rounded-0">
-                    <a href="<?php print $item['link'] ?>" class="link-img card-img-top">
+            <div class="col-lg-6">
+                <div class="card color-green text-center border-0 h-100 mx-sm-auto ml-lg-0">
                         <?php if (!isset($show_fields) or $show_fields == false or in_array('thumbnail', $show_fields)): ?>
                             <img src="<?php print $item['image']; ?>" class="img-cover" alt="">
                         <?php endif; ?>
-                    </a>
 
                     <div class="card-body">
+
                         <?php if (!isset($show_fields) or $show_fields == false or in_array('title', $show_fields)): ?>
                             <h5 class="card-title">
-                                <a href="<?php print $item['link'] ?>"><?php print $item['title'] ?></a>
+                                <?php print $item['title'] ?>
                             </h5>
                         <?php endif; ?>
 
@@ -36,8 +35,8 @@ description: news
                             <p class="card-text"><?php print $item['description'] ?></p>
                         <?php endif; ?>
                     </div>
-                    <div class="card-footer border-0 bg-transparent">
-                        <module type="btn" template="sporta-card-links-with-icons" button_style="yellow" button_text="Find out more"/>
+                    <div class="card-footer card-footer bg-transparent border-0">
+                        <module type="btn" template="sporta-buttons" button_style="btn btn-animate" button_text="Meer info"/>
                     </div>
                 </div>
             </div>

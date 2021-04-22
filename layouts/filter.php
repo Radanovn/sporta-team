a48a56c50a948c56ba8b220bee1b68efcc8e4334
Nikolay <rachkuw@gmail.com>
2021-04-22 14:37:34 +0300

    filter page

---
+++ layouts/filter.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+type: layout
+content_type: static
+name: Filter
+position: 13
+description: Filter
+
+*/
+
+?>
+<?php include template_dir() . "header.php"; ?>
+
+<div class="edit main-content" rel="content" field="sporta-team_filter">
+    <module type="layouts" template="filter/skin-1"/>
+</div>
+
+<?php include template_dir() . "footer.php"; ?>

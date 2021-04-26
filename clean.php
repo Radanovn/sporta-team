f1326b71d42404ae6f7a60cd72998028c92a10c2
Peter Ivanov <peter@microweber.com>
2021-04-26 15:37:29 +0300

    update

---
+++ clean.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+
+type: layout
+content_type: static
+name: Clean
+position: 2
+is_default: true
+description: Clean layout
+
+*/
+
+
+?>
+<?php include template_dir() . "header.php"; ?>
+
+
+
+<div class="edit" rel="content" field="content">
+
+</div>
+
+<?php include template_dir() . "footer.php"; ?>

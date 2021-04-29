2a5bedfc9a1301df4ebba1829ed8430221285035
Nikolay <rachkuw@gmail.com>
2021-04-29 14:43:23 +0300

    update

---
+++ modules/layouts/templates/club-vizit/skin-3.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+
+type: layout
+
+name: Club vizit 3
+
+position: 3
+
+*/
+
+?>
+<section class="section section-about section-custom-accompaniment edit safe-mode nodrop" field="layout-club-vizit-skin-3-<?php print $params['id'] ?>" rel="content">
+    <div class="container position-relative">
+        <div class="row no-gutters quote-card">
+            <div class="col-lg-6">
+                <div class="quote-card-content">
+                    <blockquote>
+                        <p>The Sporta manager contributed to the items that were requested from our club. Opportunities were offered that were appropriate for our club. There was a constructive atmosphere.</p>
+                        <div class="author-wrap">
+                            <div class="author__img">
+                                <img src="<?php print template_url(); ?>images/photo.jpg" class="d-block" alt="">
+                            </div>
+                            <div class="author__info">
+                                <div class="author__name">jan janssens</div>
+                                <div class="author__location">volleyclub westerlo</div>
+                            </div>
+                        </div>
+                    </blockquote>
+                </div>
+            </div>
+            <div class="col-lg-5 offset-lg-1 d-none d-lg-block">
+                <div class="quote-card__img">
+                    <img src="<?php print template_url(); ?>images/vizit-img-2.jpg" class="d-block ml-auto" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

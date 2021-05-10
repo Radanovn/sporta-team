a4714f25cd95abb6b640d713ab1a1cdd2828ba42
Nikolay <rachkuw@gmail.com>
2021-05-10 14:36:06 +0300

    update footer skin for menu module

--- modules/menu/templates/footer.php
+++ modules/menu/templates/footer.php
@@ -9,18 +9,28 @@ name: Footer
 description: Footer menu
 
 */
-
 ?>
 
 <?php
-$menu_filter['ul_class'] = "list-unstyled footer__submenu";
-
-
-$menu_filter['link'] = '<a itemprop="url" data-item-id="{id}"  href="{url}">{title}</a>';
+$menu_filter['return_data'] = true;
 $mt = menu_tree($menu_filter);
 
-if ($mt != false) {
-    print ($mt);
+if (is_array($mt)) {
+    foreach($mt as $item) {
+        if (isset($item['url'])): ?>
+            <ul class="list-unstyled footer__submenu">
+                <a itemprop='<?php print $item['url'] ?>' data-item-id='<?php print $item['id'] ?>' href='<?php print $item['url'] ?>'>
+                    <?php print $item['title'] ?>
+                </a>
+            </ul>
+
+        <?php else : ?>
+            <a>
+                <?php print $item['title'] ?>
+            </a>
+        <?php endif;
+    }
+
 } else {
     print lnotif("There are no items in the menu <b>" . $params['menu-name'] . '</b>');
 }

7f8b376900b73a163e057e3350234fbd9a106635
Nikolay <rachkuw@gmail.com>
2021-05-10 15:47:41 +0300

    update

--- modules/layouts/templates/club-support/skin-2.php
+++ modules/layouts/templates/club-support/skin-2.php
@@ -100,7 +100,7 @@ position: 2
                 </div>
             </div>
         </div>
-        <div class="section-wrapper-btn text-center cloneable">
+        <div class="section-wrapper-btn text-center">
             <module type="btn" template="sporta-buttons" button_style="btn btn-animate btn-green" button_text="View all the benefits">
         </div>
     </div>

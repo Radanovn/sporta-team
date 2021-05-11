660da4f2879c9226c4d807b87ed18a6741fcaf3b
Peter Ivanov <peter@microweber.com>
2021-05-11 17:41:50 +0300

    update

--- config.php
+++ config.php
@@ -6,4 +6,5 @@ $config['description'] = "";
 $config['author'] = "Microweber";
 $config['version'] = 1.0;
 $config['standalone_module_skins'] = true;
+$config['use_dynamic_layouts_for_posts'] = true;
 $config['framework'] = "bootstrap4";

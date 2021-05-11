4021eaeebcd0d4f0b5e8ea86f69a8af706f9eaa5
Nikolay <rachkuw@gmail.com>
2021-05-11 17:54:13 +0300

    contact form skin

---
+++ modules/contact_form/templates/sporta-safety-newsletter-form.php
@@ -0,0 +1,16 @@
+<?php
+/*
+
+  type: layout
+
+  name: Sporta newsletter
+
+  description: Sporta
+
+ */
+?>
+<form class="form-newsletter needs-validation" id="contactform" data-form-id="<?php print $form_id ?>" name="<?php print $form_id ?>" method="post" novalidate>
+    <?php print csrf_form() ?>
+    <module type="custom_fields" default-fields="Voornaam*[type=text,field_size=12,show_placeholder=true],Naam club[type=number,field_size=12,show_placeholder=true],Your.email@domain.com[type=email,field_size=12,show_placeholder=true]"/>
+    <module type="btn" button_action="submit" template="sporta-buttons" button_style="btn btn-animate btn-red" button_text="SCHRIJF IN">
+</form>

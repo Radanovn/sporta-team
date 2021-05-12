897e67259a0f7e85c59644e48b903f885a7a04de
raikov <raikov@ymail.com>
2021-05-12 14:28:40 +0300

    main menu

--- modules/menu/templates/header-main.php
+++ modules/menu/templates/header-main.php
@@ -4,94 +4,40 @@
 
 type: layout
 
-name: Footer
+name: Template for the main navigation
 
-description: Footer menu
+description: The menu which is just before content
 
 */
 
-
-?>
-
-<?php
-
 $menu_filter['return_data'] = true;
-
 $menuTree = menu_tree($menu_filter);
-
-
-/*function rendItems($items): string
-{
-    $html = '<ul>';
-    if(!isset($items) || !is_array($items)) {
-        return '';
-    }
-    foreach ($items as $item) {
-        $html .= ('<li><a href="'.$item['url'].'">' . $item['title'] . '</a>' . rendItems($item['children']) . '</li>');
-    }
-    return $html . '</ul>';
-}*/
-
-
-
-
-
-if(!function_exists('rendItemsSporta')) {
-    function rendItemsSporta($items, $level = -1): string
+if(!function_exists('headerSportaMenuItem')) {
+    function headerSportaMenuItem($items, $level = -1): string
     {
-
-
         $level++;
-        $ulClass = $level === 0 ? 'list-unstyled footer__menu' : 'list-unstyled footer__submenu';
-        $aClass = $level === 0 ? 'footer__menu-link' : '';
-        $html = '<ul class="'.$ulClass.'">';
         if(!isset($items) || !is_array($items)) {
             return '';
         }
-        foreach ($items as $item) {
-            $html .= ('<li><a class="'.$aClass.'" href="'.$item['url'].'">' . $item['title'] . '</a>' . rendItemsSporta($item['children'], $level) . '</li>');
+        $html = '';
+        if ($level === 0) {
+            foreach ($items as $item) {
+                $html .= '<a class="dropdown-item" href="'.$item['url'].'">';
+                if($item['default_image']) {
+                    $html .= '<span class="dropdown-item__icon"><img alt="" src="'.$item['default_image'].'"></span>';
+                }
+                $html .= '<span>'.$item['title'].'</span></a>';
+                $html .= headerSportaMenuItem($item['children'], $level);
+            }
+        } else {
+            $html .= '<ul class="list-unstyled dropdown-item__sublist">';
+            foreach ($items as $item) {
+                $html .= ('<li><a href="'.$item['url'].'">' . $item['title'] . '</a>' . headerSportaMenuItem($item['children'], $level) . '</li>');
+            }
+            $html .= '</ul>';
         }
-        return $html . ($level > 0 ? '</ul>' : '');
+        return $html;
     }
 }
-
-$contactBlock = '<div class="contact-block edit" rel="module" field="module-'.$params['id'].'"> <a href="#" class="footer__menu-link">Get in touch</a> <a href="mailto:Federatie@sporta.be" class="st-email">Federatie@sporta.be</a> <a href="tel:+32033615340" class="st-phone">+32 (0) 3 361 53 40</a><p class="st-address">Boomgaardstraat 22 Bus 50 B-2600 Berchem</p> <a href="#" class="btn btn-animate btn-red">Contact us</a></div>';
-
-print is_array($menuTree) ? rendItemsSporta($menuTree) : lnotif("There are no items in the menu <b>" . $params['menu-name'] . '</b>');
-print is_array($menuTree) ? ('<li>' . $contactBlock . '</li></ul>') : $contactBlock;
-
-
-
+  is_array($menuTree) ? print headerSportaMenuItem($menuTree) : print lnotif("There are no items in the menu <b>" . $params['menu-name'] . '</b>')
 ?>
-
-
-<div class="dropdown-menu w-100 megamenu">
-    <div class="row justify-content-between flex-xl-nowrap">
-        <div class="megamenu__column">
-            <a class="dropdown-item" href="#">
-                                        <span class="dropdown-item__icon">
-                                            <img src="<?php print template_url(); ?>modules/layouts/svg/header/skin-4.svg">
-                                        </span>
-                <span>Insurance</span>
-            </a>
-            <ul class="list-unstyled dropdown-item__sublist">
-                <li>
-                    <a href="#">Our sports insurance</a>
-                </li>
-                <li>
-                    <a href="#">Additional options</a>
-                </li>
-                <li>
-                    <a href="#">What to do in the event of an accident?</a>
-                </li>
-            </ul>
-
-
-        </div>
-
-
-
-    </div>
-</div>
-
-

f1112745edfab3599e8e8942e0b33d11f160af47
Nikolay <rachkuw@gmail.com>
2021-06-02 13:42:29 +0300

    update

---
+++ elements/skin-8.php
@@ -0,0 +1,8 @@
+<div class="row no-gutters edit safe-mode allow-drop" field="layout-news-inner-skin-8-<?php print $params['id'] ?>" rel="content">
+    <div class="col-lg-8 col-xl-6 mx-lg-auto">
+        <div class="single-post__socials">
+            <div class="single-post__socials-title">Schare this article</div>
+                <module type="social_links" template="sporta-socials-news-page"/>
+        </div>
+    </div>
+</div>

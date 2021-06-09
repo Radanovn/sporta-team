a80f148d2e6a564a811c516cd1ce513163c235e2
raikov <raikov@ymail.com>
2021-06-09 13:37:54 +0300

    skin 5 fix

--- modules/blog/src/resources/views/index.blade.php
+++ modules/blog/src/resources/views/index.blade.php
@@ -4,22 +4,7 @@ $randId = uniqid();
 
 {!! $posts->scripts() !!}
 
-<section class="section section-filter section-calendar-trainings edit safe-mode nodrop" field="layout-blog-{{$randId}}" rel="content">
-    <div class="container position-relative">
-        <div class="circles circles--top-right d-none d-lg-block">
-            <img src="<?php print template_url(); ?>modules/layouts/svg/calendar/skin-6.svg">
-        </div>
-        <div class="circles circles--bottom-left d-none d-xl-block">
-            <img src="<?php print template_url(); ?>modules/layouts/svg/calendar/skin-7.svg">
-        </div>
-        <div class="row">
-            <div class="col-lg-9 mx-lg-auto">
-                <div class="section__block-title text-sm-center">
-                    <h2>Blog</h2>
-                    <p class="lead-text">This is the simple blog</p>
-                </div>
-            </div>
-        </div>
+
 {{--
         <div class="row">
             <div class="col-md-8"></div>
@@ -141,5 +126,4 @@ $randId = uniqid();
             </div>
 
         </div>
-    </div>
-</section>
+

fce4c96b114b550c5c5d0144d6d91f0c7efb9c5e
Nikolay <rachkuw@gmail.com>
2021-06-15 14:52:13 +0300

    update

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php include template_dir() . "header.php"; ?>
+
+
+<section class="section section-error-404">
+    <div class="container">
+        <div class="text-center">
+            <img src="<?php print template_url(); ?>images/404-img.svg" alt="">
+            <h4>Something went wrong (</h4>
+            <p>Page not found, try going pack to home page.</p>
+            <module type="btn"  url="<?php print site_url(); ?>" template="sporta-buttons" button_style="btn btn-animate btn-red" button_text="Back to home"/>
+        </div>
+    </div>
+</section>
+
+
+<?php include template_dir() . "footer.php"; ?>

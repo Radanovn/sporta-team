f2bc260396a0d1a2be21423f84975f3cde898d70
Nikolay <rachkuw@gmail.com>
2021-06-30 13:51:40 +0300

    update

---
+++ modules/layouts/templates/calendar/skin-5.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+
+type: layout
+
+name: Calendar 5
+
+position: 5
+
+*/
+
+?>
+
+<section class="section section-filter section-calendar-trainings edit safe-mode nodrop" field="layout-blog-<?php print $params['id'] ?>" rel="content">
+    <div class="container position-relative">
+        <module type="blog"/>
+    </div>
+</section>

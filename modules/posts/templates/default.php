402e266a2337bb1cfac37300d2fed30304c6ae27
Nikolay <rachkuw@gmail.com>
2021-07-06 11:24:50 +0300

    update

--- modules/posts/templates/default.php
+++ modules/posts/templates/default.php
@@ -39,10 +39,13 @@ description: Grid Columns
                         <?php endif; ?>
                     </div>
                     <div class="card-footer border-0 bg-transparent">
-                        <a class="card-link link color-red" href="<?php print $item['link'] ?>" ><?php echo $read_more_text;?></a>
-                        <span class="link__arrow">
-                            <img src="<?php print template_url(); ?>modules/layouts/svg/news/skin-2.svg">
-                        </span>
+                        <a class="card-link link <?php echo $colorClass ?>" href="<?php print $item['link'] ?>" > <span><?php echo $read_more_text;?></span>
+                            <span class="link__arrow">
+                                    <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1.70711 0.370774C1.31658 -0.0197506 0.683417 -0.0197506 0.292893 0.370774C-0.0976315 0.761298 -0.0976314 1.39446 0.292893 1.78499L1.70711 0.370774ZM5 5.07788L5.70711 5.78499C5.89464 5.59745 6 5.3431 6 5.07788C6 4.81266 5.89464 4.55831 5.70711 4.37077L5 5.07788ZM0.292893 8.37077C-0.0976311 8.7613 -0.0976311 9.39446 0.292893 9.78499C0.683418 10.1755 1.31658 10.1755 1.70711 9.78499L0.292893 8.37077ZM0.292893 1.78499L4.29289 5.78499L5.70711 4.37077L1.70711 0.370774L0.292893 1.78499ZM4.29289 4.37077L0.292893 8.37077L1.70711 9.78499L5.70711 5.78499L4.29289 4.37077Z" fill="#000000"/>
+                                    </svg>
+                                </span>
+                        </a>
                     </div>
                 </div>
             <?php endforeach; ?>

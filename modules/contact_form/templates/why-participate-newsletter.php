2cfd655d7a79175048a4c1cec90b0ea0b63e1bdb
Nikolay <rachkuw@gmail.com>
2021-07-07 13:55:38 +0300

    update

--- modules/contact_form/templates/why-participate-newsletter.php
+++ modules/contact_form/templates/why-participate-newsletter.php
@@ -10,7 +10,7 @@
  */
 ?>
 
-<form class="form-information-package needs-validation edit" field="contact-form-whyParticipate" rel="global" id="<?php print $form_id ?>" novalidate data-form-id="<?php print $form_id ?>" name="<?php print $form_id ?>" method="post">
+<form class="form-information-package needs-validation" id="<?php print $form_id ?>" novalidate data-form-id="<?php print $form_id ?>" name="<?php print $form_id ?>" method="post">
     <?php print csrf_form() ?>
     <module type="custom_fields" default-fields="Your.email@domain.com[type=email,field_size=12,show_placeholder=true]"/>
 

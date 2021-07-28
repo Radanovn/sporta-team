89eeb10b38a3244c4c3cf4b393abe88b3a74fa22
Nikolay <rachkuw@gmail.com>
2021-07-28 18:45:27 +0300

    update

--- modules/contact_form/templates/sporta-news-page-form.php
+++ modules/contact_form/templates/sporta-news-page-form.php
@@ -15,8 +15,7 @@
     <module type="btn" button_action="submit" class="cloneable nodrop" template="sporta-buttons" button_style="btn btn-animate btn-red" button_text="SCHRIJF IN">
 
 
-        <div style="display: none" class="message-sent" id="msg<?php print $form_id ?>">
+        <div style="display: none" class="message-sent  " id="msg<?php print $form_id ?>">
             <span class="message-sent-icon message-sent-icon-orange"></span>
-            <p><?php _e("Your Email was sent successfully"); ?> </p>
         </div>
 </form>
